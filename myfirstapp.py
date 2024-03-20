5403bd557e8e1fb51b3927a29fb849055a00d168
rasydigital <164140237+rasydigital@users.noreply.github.com>
2024-03-20 14:55:21 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -3,7 +3,35 @@ import numpy as np
 import pandas as pd
 
 st.header("My first Streamlit App")
-st.write(pd.DataFrame({
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['line chart','map','T n C'])
+
+if option=='line chart':
+    chart_data = pd.DataFrame(
+      np.random.randn(20, 3),
+      columns=['a', 'b', 'c'])
+    st.line_chart(chart_data)
+
+elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [3.1242, 101.6861],
+    columns=['lat', 'lon'])
+    st.map(map_data)
+
+elif option=='T n C':
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+          'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+          'Attendance Status': ['yes', 'yes', 'yes', 'no']
+        }))
+
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
     'Students': ['John', 'Lofa', 'Siti', 'Amy'],
     'Attendance Status': ['yes', 'yes', 'yes', 'no']
-}))
+    }))
