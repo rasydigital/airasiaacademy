17b0c1f719de8b5b97e16a49089f3fc54e8cf171
rasydigital <164140237+rasydigital@users.noreply.github.com>
2024-03-21 10:03:48 +0800

    Update rasy.py

--- rasy.py
+++ rasy.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
 
+st.header("Advertising Sale")
+option = st.sidebar.selectbox(
+    'Select Option',
+     ['line chart','map','T n C','Long Process'])
