cab376df00a7dbbac7803206b8bfb9ce5af39a02
rasydigital <164140237+rasydigital@users.noreply.github.com>
2024-03-21 11:30:30 +0800

    Update 4_8_project_deployment_rasy.py

--- 4_8_project_deployment_rasy.py
+++ 4_8_project_deployment_rasy.py
@@ -11,9 +11,9 @@ st.write("This app predicts the **Sales** Advertising!")
 st.sidebar.header('User Input Parameters') 
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.5, 200.0, 300.0) 
-    Radio = st.sidebar.slider('Radio', 0.5, 4.4, 49.0)
-    Newspaper = st.sidebar.slider('Newspaper', 0.5, 30.0, 114.0)
+    TV = st.sidebar.slider('TV', 1.0, 5.0, 10.0) 
+    Radio = st.sidebar.slider('Radio', 1.0, 5.0, 10.0)
+    Newspaper = st.sidebar.slider('Newspaper', 1.0, 5.0, 10.0)
     
     data = {'TV': TV, 
             'Radio': Radio,
