196c398a6088e96289227bf1c9e20e2ca952ae19
rasydigital <164140237+rasydigital@users.noreply.github.com>
2024-03-21 11:15:20 +0800

    Update 4_8_project_deployment_rasy.py

--- 4_8_project_deployment_rasy.py
+++ 4_8_project_deployment_rasy.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import LinearRegression
+from sklearn.linear_model import LinearRegression
 st.write("# Advertising Sale Prediction")
 st.write("This app predicts the **Sales** Advertising!")
 
